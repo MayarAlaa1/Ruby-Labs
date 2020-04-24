cfc81a0274c07d984cb2f96a098db5626b4d25d9
MayarAlaa1 <mayar.alaa188@gmail.com>
2020-04-24 21:43:58 +0200

    formatting output tp print

--- lab1.rb
+++ lab1.rb
@@ -11,18 +11,18 @@ class ComplexNumbers
         @real = real
         @imaginary = imaginary
     end
-##########################################
+############### Plus ###################
     def +(num)
         @@Stats["+(num)"] +=1
         ComplexNumbers.new(@real + num.real, @imaginary+ num.imaginary)
     end
-#########################################
+################ Mutiply ####################
     def *(num)
         @@Stats["*(num)"] +=1
         ComplexNumbers.new(@real * num.real - @imaginary * num.imaginary,@real*num.imaginary+ @imaginary * num.real)
     end
 
-########################################
+############  Bulk addition ###################
     def self.bulk_add(cns)
         @@Stats["bulk_add"] +=1
         add=ComplexNumbers.new(0,0)
@@ -31,7 +31,7 @@ class ComplexNumbers
         end
         add
     end
-##########################################
+############# Bulk multiplication ##############
     def self.bulk_multiply(cns)
         @@Stats["bulk_multiply"] +=1
         mul=ComplexNumbers.new(1,0)
@@ -40,12 +40,17 @@ class ComplexNumbers
         end
         mul
     end
-#########################################
+############ Stats ############################
     def self.get_stats()
       
         puts @@Stats
 
     end 
+########## Formatting output ################
+
+    def to_s
+        "#{self.real}+#{self.imaginary} i"
+    end
 ##########################################
 end    
 
@@ -53,16 +58,11 @@ comp1= ComplexNumbers.new(1,2)
 comp2= ComplexNumbers.new(3,4)
 comp3= ComplexNumbers.new(5,6)
 
-sum = comp1 + comp2
-multiplication = comp1 * comp2
-bulk_sum= ComplexNumbers.bulk_add([comp1,comp2,comp3])
-bulk_mul= ComplexNumbers.bulk_multiply([comp1,comp2,comp3])
-
 ########### print ########################
-puts "sum= #{sum.real} + #{sum.imaginary} i" 
-puts "Multiplication= #{multiplication.real} + #{multiplication.imaginary} i" 
-puts "Bulk addition= #{bulk_sum.real} + #{bulk_sum.imaginary} i" 
-puts "Bulk Multiplication = #{bulk_mul.real} + #{bulk_mul.imaginary} i" 
+puts "sum = #{ comp1 + comp2}"
+puts"multiplication = #{comp1 * comp2}"
+puts "bulk_sum = #{ComplexNumbers.bulk_add([comp1,comp2,comp3])}"
+puts "bulk_mul = #{ComplexNumbers.bulk_multiply([comp1,comp2,comp3])}"
 puts ComplexNumbers.get_stats
 
 
